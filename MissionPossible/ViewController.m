22f20aa4f471d4e6e64a29ffbbaf52015337c436
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-24 17:50:28 -0400

    fully functioning

--- MissionPossible/ViewController.m
+++ MissionPossible/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (strong, nonatomic) IBOutlet UIButton *counterButton;
+- (IBAction)buttonPressed:(UIButton *)sender;
 
 @end
 
@@ -26,5 +26,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)buttonPressed:(UIButton *)sender {
+    
+    NSString *senderTitle = sender.currentTitle;
+    
+    NSInteger counter = senderTitle.integerValue;
+    counter++;
+    
+    NSString *stringCounter = [NSString stringWithFormat:@"%@", @(counter)];
+    [sender setTitle:stringCounter forState:UIControlStateNormal];
+}
 
 @end
